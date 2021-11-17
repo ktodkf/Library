51f7d3ed64d0423ffdaf191e7202b09d56a03c7d
Yung Byd <degterev88viktor@gmail.com>
2021-11-17 23:55:07 +0300

    1

--- SQL/create.sql
+++ SQL/create.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS users
 (
 id_users INT PRIMARY KEY AUTO_INCREMENT,
-email VARCHAR(80) UNIQUE,
-login VARCHAR(80) UNIQUE,
+email VARCHAR(80) UNIQUE NOT NULL,
+login VARCHAR(80) UNIQUE NOT NULL,
 pass VARCHAR(16)
 );
 
 CREATE TABLE IF NOT EXISTS authors
 (
 id_authors INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(80) UNIQUE
+name VARCHAR(80) NOT NULL UNIQUE
+    
 );
-
 CREATE TABLE IF NOT EXISTS books_authors
 (
 id_book INT,
@@ -22,7 +22,7 @@ PRIMARY KEY (id_book, id_author)
 CREATE TABLE IF NOT EXISTS ganres
 (
 id_ganre INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(80) UNIQUE
+name VARCHAR(80) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS books_ganres
@@ -35,11 +35,9 @@ PRIMARY KEY (id_ganre, id_book)
 CREATE TABLE IF NOT EXISTS books
 (
 id_book INT PRIMARY KEY AUTO_INCREMENT,
-id_autors INT NOT NULL,
-id_ganre INT NOT NULL,
-title VARCHAR(80) UNIQUE,
-cost INT,
-rating INT
+title VARCHAR(80) NOT NULL UNIQUE,
+cost INT DEFAULT (0),
+rating INT DEFAULT (0)
 );
 
 CREATE TABLE IF NOT EXISTS review
@@ -73,7 +71,7 @@ id_order INT NOT NULL
 CREATE TABLE IF NOT EXISTS oreders_status
 (
 id_status INT PRIMARY KEY AUTO_INCREMENT,
-name varchar(50)	
+name varchar(50) NOT NULL	
 );
 
 CREATE TABLE IF NOT EXISTS orders
@@ -81,7 +79,7 @@ CREATE TABLE IF NOT EXISTS orders
 id_orders INT PRIMARY KEY AUTO_INCREMENT,
 id_users INT NOT NULL,
 id_status INT NOT NULL,
-order_time DATETIME,
-total_sum INT,
-total_amount INT
+order_time DATETIME NOT NULL DEFAULT(2000/01/01 00.00.00),
+total_sum INT NOT NULL,
+total_amount INT 
 );
