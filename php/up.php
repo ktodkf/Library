5853c030a34790dceb95b59fe4ce4ace318a23c0
Yung Byd <degterev88viktor@gmail.com>
2021-12-02 15:45:52 +0300

    Update up.php

--- php/up.php
+++ php/up.php
@@ -9,9 +9,6 @@
 	} else if (mb_strlen($login) < 3 || mb_strlen($login) > 50) {
 		echo "Недопустимая длина имени";
 		exit();
-	} else if (mb_strlen($pass) < 2 || mb_strlen($pass) > 6) {
-		echo "Недопустимая длина пароля (от 2 до 6 символов)";
-		exit();
 	}
 
 	$pass= md5($pass."ghjkl12dsgfhd34");
